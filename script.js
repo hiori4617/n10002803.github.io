65e160289aa261934d0dede8229fc6196a3a02d9
Hiori <hiori1103@gmail.com>
2026-03-25 13:44:25 -0400

    save changes

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Start
+document.querySelector(".start").onclick = function() {
+  document.querySelector(".video-box").innerText = "Connecting...";
+}
 
+// Next
+document.querySelector(".next").onclick = function() {
+  document.querySelector(".video-box").innerText = "New User Connected";
+}
+
+// Stop
+document.querySelector(".stop").onclick = function() {
+  document.querySelector(".video-box").innerText = "Disconnected";
+}
